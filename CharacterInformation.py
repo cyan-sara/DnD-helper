c71829c710bbccfe3d397dacef7eb3f50f075818
bcb14a <bcb14a@acu.edu>
2016-04-13 09:26:20 -0500

    Character Information

---
+++ CharacterInformation.py
@@ -0,0 +1,21 @@
+# Character Information
+# Using file;Dictionaries 
+# Goal is to turn all the sheets into files'
+#Lower cases Key Values
+#Would you like input the story of your character? (File input)
+
+
+def main():
+    D1=[]
+    Print(" Informtion")
+    name=input("Enter Character Name: ")
+    Class=input("Enter Class: ")
+    LvL=int(input("Level: "))
+    race=input("Race: ")
+    Alin=input("Alignment: ")
+    EXP=int(input("Experience Points: "))
+    PN=input("Player Name: ")
+    D1["name"]= name
+
+main()
+
