452481292cf37bff017d0518f7ab130455ed8ee5
bcb14a <bcb14a@acu.edu>
2016-04-14 17:38:30 -0500

    Npc Updates

--- CharacterInformation.py
+++ CharacterInformation.py
@@ -3,14 +3,28 @@
 # Goal is to turn all the sheets into files'
 #Lower cases Key Values
 #Would you like input the story of your character? (File input)
+#Ask if NPC or Playable Character
+
 
 
 def main():
     D1={}    
     while True:
         yn=input("Would you like to create a character? (y/n) ")
+        while True:
+            sel=input("Is the character a NPC? (y/n) ")
+            if sel=="y": #NPC Info
+                name=input("Enter Character Name: ")
+                Class=input("Enter Class: ")
+                LvL=int(input("Level: "))
+                race=input("Race: ")
+                Alin=input("Alignment: ")
+                EXP=int(input("Experience Points: "))
+                print("")
+            if sel=="n":
+                break
         if yn=="n":
-            break
+            break #Playable Info
         name=input("Enter Character Name: ")
         Class=input("Enter Class: ")
         LvL=int(input("Level: "))
