d6c8a6cfbf5fc3b04795dad488509b46a6772457
Raquel Dottle <redquitobud@gmail.com>
2016-04-14 17:31:54 -0500

    removed unnecesssary character create function

--- dndhelp_main.py
+++ dndhelp_main.py
@@ -8,26 +8,9 @@ import #Sara
 import #Bianca
 
 def main():
-	print("This program serves as a helper for Dungeons and Dragons DMs.")
-	version = input("Please select your version number: ") #this will affect the books that we have. Might get tossed if we can't do books
-	characterselect()
+	print("This program serves as a helper for Dungeons and Dragons (version 5) DMs.")
 	turncount = turn_begin()
-	
-	
-	
-	
-def characterselect(): #selects characters to be used. Allows addition of new chars (executes Bianca's module)
-	print("Enter the characters that are going on this encounter. Enter \"end\" to show the end of the list.")
-	characters = []
-	while True:
-		x = input("")
-		if x == "end":
-			break
-		characters.append(x)
-	try: #use list "characters" to grab files/execute new char creator
-	except IOError:
-		print("Character \"", x, "\" not found. Would you like to add a new character?")
-		
+			
 def turn_begin(): #keeps track of turns for status effect purposes, etc
 	yn = input("Are you ready to start the encounter? (y/n)")
 	count = 0
