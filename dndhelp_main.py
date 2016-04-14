7dc719e20962f4f79cf178b2fe6a228ecd3a6ada
Raquel Dottle <redquitobud@gmail.com>
2016-04-14 18:50:13 -0500

    main is basically done!

--- dndhelp_main.py
+++ dndhelp_main.py
@@ -3,13 +3,29 @@
 	#turn tracker,
 	#data retrieval on characters
 
-import #Tanner
-import #Sara
-import #Bianca
+#Tanner
+#Sara
+#Bianca
 
 def main():
 	print("This program serves as a helper for Dungeons and Dragons (version 5) DMs.")
 	turncount = turn_begin()
+	while True:
+		x= input("What would you like to do next? Add character, roll dice, end turn, end encounter. ")
+		x = x.lower().rstrip()
+		if x=="end turn":
+			turncount += 1
+			print("Current turn:", turncount)
+		elif x == "add character":
+			#execute char creator
+		elif x == "roll dice":
+			#execute dice roller
+		elif x == "end encounter":
+			break
+		else:
+			print("Invalid command.")
+	print("Goodbye!")
+	
 			
 def turn_begin(): #keeps track of turns for status effect purposes, etc
 	yn = input("Are you ready to start the encounter? (y/n)")
