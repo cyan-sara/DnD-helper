f493caf663269766395d9d2e06c0d39e285e5c27
bcb14a <bcb14a@acu.edu>
2016-04-18 09:17:36 -0500

    Character Function

--- CharacterInformation.py
+++ CharacterInformation.py
@@ -7,7 +7,7 @@
 
 
 
-def CharaInput():
+def PlayableChara(player):
     Player={}
     print("\nYou are creating a playable character.")
     name=input("Enter Character Name: ")
@@ -21,5 +21,19 @@ def CharaInput():
     print("")
     print(Player)
 
-    return 
+    return Player
 
+def NPCChara(NPC):
+    print("\nYou are creating a non-playable character.")
+    name=input("Enter Character Name: ")
+    Class=input("Enter Class: ")
+    LvL=int(input("Level: "))
+    race=input("Race: ")
+    Alin=input("Alignment: ")
+    EXP=int(input("Experience Points: "))
+    PN=input("Player Name: ")
+    Player["name"]= name
+    print("")
+    print(Player)
+
+    return NPC
