f66d2c9e9dbd16a19e88acaca08fca1d5d023a0b
bcb14a <bcb14a@acu.edu>
2016-04-18 09:08:37 -0500

    Update3?

--- CharacterInformation.py
+++ CharacterInformation.py
@@ -7,34 +7,19 @@
 
 
 
-def main():
-    D1={}    
-    while True:
-        yn=input("Would you like to create a character? (y/n) ")
-        while True:
-            sel=input("Is the character a NPC? (y/n) ")
-            if sel=="y": #NPC Info
-                name=input("Enter Character Name: ")
-                Class=input("Enter Class: ")
-                LvL=int(input("Level: "))
-                race=input("Race: ")
-                Alin=input("Alignment: ")
-                EXP=int(input("Experience Points: "))
-                print("")
-            if sel=="n":
-                break
-        if yn=="n":
-            break #Playable Info
-        name=input("Enter Character Name: ")
-        Class=input("Enter Class: ")
-        LvL=int(input("Level: "))
-        race=input("Race: ")
-        Alin=input("Alignment: ")
-        EXP=int(input("Experience Points: "))
-        PN=input("Player Name: ")
-        print("")
-        
-    print(D1)
+def CharaInput():
+    Player={}
+    print("\nYou are creating a playable character.")
+    name=input("Enter Character Name: ")
+    Class=input("Enter Class: ")
+    LvL=int(input("Level: "))
+    race=input("Race: ")
+    Alin=input("Alignment: ")
+    EXP=int(input("Experience Points: "))
+    PN=input("Player Name: ")
+    Player["name"]= name
+    print("")
+    print(Player)
 
-main()
+    return 
 
